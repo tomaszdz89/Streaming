026e60e71e3edc66b56c613e18129c094c5bf344
rrhaya <frontheale1@interia.pl>
2022-12-22 11:16:48 +0100

    fix: Remove not used component

--- src/components/IconTextField.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TextField from '@mui/material/TextField'
-
-const IconTextField = (props: any): JSX.Element => {
-  return (<TextField {...props} />)
-}
-
-export default IconTextField
