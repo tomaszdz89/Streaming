751eed3e8444e9e2ef3fe8db57044cbc2ddd5854
rrhaya <frontheale1@interia.pl>
2022-12-26 19:25:16 +0100

    feat: Add form validation on login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
-import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
 import Visibility from '@mui/icons-material/Visibility'
@@ -11,11 +10,35 @@ import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 const Login = (): JSX.Element => {
   const [showPassword, setShowPassword] = useState(false)
-
   const handleClickShowPassword = (): void => setShowPassword((show) => !show)
+
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email('Enter a valid email')
+      .required('Email is required'),
+    password: yup
+      .string()
+      .min(8, 'Password must be at least 8 characters')
+      .max(15, 'Password must be less than 15 characters')
+      .required('Password is required')
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
 
   return (
     <div
@@ -52,41 +75,53 @@ const Login = (): JSX.Element => {
               Don&apos;t have an account? <Link to="/register">Sign up</Link>
             </Typography>
           </Box>
-          <Box component="form">
+          <form onSubmit={formik.handleSubmit}>
             <Grid container direction="column" gap="10px">
-              <FormControl style={{ width: '100%' }}>
-                <TextField label="Email Address"></TextField>
-              </FormControl>
-              <FormControl style={{ width: '100%' }}>
-                <TextField
-                  label="Password"
-                  type={showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={handleClickShowPassword}>
-                          {showPassword ? <Visibility /> : <VisibilityOff />}
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                  }}
-                ></TextField>
-              </FormControl>
+              <TextField
+                label="Email Address"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+                error={Boolean(formik.touched.email) && Boolean(formik.errors.email)}
+              ></TextField>
+              {(Boolean(formik.touched.email) && Boolean(formik.errors.email)) ? <Typography color="error" variant="body2">{formik.errors.email}</Typography> : null}
+              <TextField
+                label="Password"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                error={Boolean(formik.touched.password) && Boolean(formik.errors.password)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleClickShowPassword}>
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              ></TextField>
+              {(Boolean(formik.touched.password) && Boolean(formik.errors.password)) ? <Typography color="error" variant="body2">{formik.errors.password}</Typography> : null}
               <Button
                 variant="contained"
+                color="primary"
                 style={{
                   marginTop: '10px',
                   width: '100%',
                   height: '56px',
-                  background: '#e1e1e1',
-                  color: '#808080',
                   textTransform: 'capitalize'
                 }}
+                type="submit"
+                disabled={!(formik.isValid && formik.dirty)}
               >
                 Continue
               </Button>
             </Grid>
-          </Box>
+          </form>
         </Box>
       </Paper>
       <Typography
