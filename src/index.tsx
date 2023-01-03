0eb2b4c375714d576901d4f87f444f60547ad723
rrhaya <frontheale1@interia.pl>
2023-01-03 12:32:40 +0100

    feat: Add axios and react redux with basic store

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
